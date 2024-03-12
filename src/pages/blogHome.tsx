8b32f5e296905281029c04e6d7a033fefea74065
Jacob Johnson <jjohnso1@mem-ins.com>
2024-03-12 16:56:23 -0500

    switching blog home and blog1 contents

--- src/pages/blogHome.tsx
+++ src/pages/blogHome.tsx
@@ -5,7 +5,6 @@ import { posts } from "../data";
 import Link from 'next/link';
 
 export default function Home() {
-
     return (
         <div className="Home">
             <header className="Home-header">
@@ -13,6 +12,7 @@ export default function Home() {
                     <div>
                         <nav className="navBarTab">
                             <Link className="fa fa-home" href='/'></Link>
+                            <p>Blog</p>
                         </nav>
                         <div className="underHeader">
                             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
@@ -26,7 +26,7 @@ export default function Home() {
                     </div>
                 </header>
             </header>
-            <div>
+            <div id='About'>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
@@ -34,6 +34,14 @@ export default function Home() {
                     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
+            <section id="Blog">
+                <div className='posts'>
+                    <Link href={'/blog1'}>
+                        <p>Emergency Book?</p>
+                        <img src="/organizedBinder.jpg" className='postPic' />
+                    </Link>
+                </div>
+            </section>
         </div>
     )
 }
