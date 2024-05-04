e5abb8bf5985ba216803045ebf528a986c24476d
Jacob Johnson <jjohnso1@mem-ins.com>
2024-05-03 21:05:39 -0500

    updating cat stats

--- src/pages/blog4.tsx
+++ src/pages/blog4.tsx
@@ -23,6 +23,7 @@ export default function Home() {
             <div className='blogFrame'>
                 <div className='blogText'>
                     <h1>Cat Stats</h1>
+                    <h5>Last updated: </h5>
                     <p>
                         This Spring/Summer, my wife & I are fostering some kittens for the local shelter.
                         This mostly just entails feeding them so they can get up to weight (a massive 2.0LB),
