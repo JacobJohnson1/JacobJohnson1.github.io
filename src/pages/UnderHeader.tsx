a282cfd78b0ec3ace415b3d945bc9c2b15d28cf9
JacobJohnson1 <70165217+JacobJohnson1@users.noreply.github.com>
2024-09-05 22:14:14 -0500

    fixed github link typo

--- src/pages/UnderHeader.tsx
+++ src/pages/UnderHeader.tsx
@@ -10,7 +10,7 @@ export default function UnderHeader() {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
             <div className="insideFooter">
                 <a href="https://www.linkedin.com/in/jacob-johnson-05bb79169/" target="blank" className="fa fa-linkedin fa-xs" id="socialIcons" />
-                <a href="hhttps://github.com/JacobJohnson1" target="blank" className="fa fa-github fa-xs" id="socialIcons" />
+                <a href="https://github.com/JacobJohnson1" target="blank" className="fa fa-github fa-xs" id="socialIcons" />
                 <a href="https://stackoverflow.com/users/15369768/jake-johnson" target="blank" className="fa fa-stack-overflow fa-xs" id="socialIcons" />
                 <a href="mailto: therealjakejohnson@gmail.com" className="fa fa-envelope-o fa-xs" id="socialIcons" />
                 <a href="https://www.goodreads.com/user/show/108357320-jacob-johnson" id="socialIcons" target='blank'>
