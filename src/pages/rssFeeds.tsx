bd84f86121d8001e676bb12dd447e2c5fc83fd64
Jacob Johnson <jjohnso1@mem-ins.com>
2025-05-09 10:57:34 -0500

    moving rss feed to its own component - makes mobile version  more friendly

---
+++ src/pages/rssFeeds.tsx
@@ -0,0 +1,30 @@
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import React, { useState, useEffect } from "react";
+import { posts } from "../data";
+import Link from 'next/link';
+import UnderHeader from './UnderHeader';
+import BlogComponent from '../components/BlogComponent';
+import GoodReadsRss from '@/components/GoodReadsRss';
+
+export default function Home() {
+
+    return (
+        <div className="Home">
+            <header className="Home-header">
+                <div>
+                    <nav className="navBarTab">
+                        <Link id="navBtn" href='/'>Home</Link>
+                    </nav>
+                    <UnderHeader />
+                </div>
+            </header>
+            <div id='FirstThingOnPage'>
+                <section id="BlogPage">
+                    <GoodReadsRss />
+                </section>
+            </div>
+        </div >
+    )
+}
+
