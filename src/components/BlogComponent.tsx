6ae515917688ff5a9a1cea6d161c374f1962adf7
JacobJohnson1 <70165217+JacobJohnson1@users.noreply.github.com>
2025-11-28 20:28:52 -0600

    making blog posts a little less embarassing

--- src/components/BlogComponent.tsx
+++ src/components/BlogComponent.tsx
@@ -16,24 +16,12 @@ export default function BlogComponent() {
                     <img src="happyHarold.jpg" className='postPic' />
                 </Link>
             </div>
-            <div className='posts'>
-                <a href={'/blog7'} className="jobPrepPost">
-                    <div className="postTitle">Job Prep</div>
-                    <img src="businessStressMan.jpg" className='postPic' />
-                </a>
-            </div>
             {/* <div className='posts'>
                 <Link href={'/blog6'}>
                     <div>Salon Website</div>
                     <img src="" className='postPic' />
                 </Link>
             </div> */}
-            <div className='posts'>
-                <Link href={'/blog4'}>
-                    <div className="postTitle">Cat Stats</div>
-                    <img src="smartCat.jpeg" className='postPic' />
-                </Link>
-            </div>
             <div className='posts'>
                 <Link href={'/blog1'}>
                     <div className="postTitle">Emergency Book?</div>
@@ -46,18 +34,12 @@ export default function BlogComponent() {
                     <img src="/sound-board.jpg" className='postPic' />
                 </Link>
             </div> */}
-            <div className='posts'>
+            {/* <div className='posts'>
                 <Link href={'/blog3'}>
                     <div className="postTitle">Poker Simulation</div>
                     <img src="/robotPoker.jpeg" className='postPic' />
                 </Link>
-            </div>
-            <div className='posts'>
-                <Link href={'/blog5'}>
-                    <div className="postTitle">Simulated Annealing</div>
-                    <img src="simAnnealing.jpg" className='postPic' />
-                </Link>
-            </div>
+            </div> */}
         </div>
     )
 }
