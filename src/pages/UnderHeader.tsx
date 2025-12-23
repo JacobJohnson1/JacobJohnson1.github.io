f4457d3c8449b8c6f14fdcde13a0ba5589ae3d5f
JacobJohnson1 <70165217+JacobJohnson1@users.noreply.github.com>
2025-12-23 14:22:03 -0600

    updated YT channel name

--- src/pages/UnderHeader.tsx
+++ src/pages/UnderHeader.tsx
@@ -7,7 +7,7 @@ export default function UnderHeader() {
             <div className="insideFooter">
                 <a href="https://www.linkedin.com/in/jacob-johnson-05bb79169/" target="blank" className="fa fa-linkedin fa-xs" id="socialIcons" />
                 <a href="https://github.com/JacobJohnson1" target="blank" className="fa fa-github fa-xs" id="socialIcons" />
-                <a href="https://www.youtube.com/@jakejohnson6298" target="blank" className="fa fa-youtube-play fa-xs" id="socialIcons" />
+                <a href="https://www.youtube.com/@jacob-johnson2" target="blank" className="fa fa-youtube-play fa-xs" id="socialIcons" />
                 <a href="mailto: therealjakejohnson@gmail.com" className="fa fa-envelope-o fa-xs" id="socialIcons" />
                 <a href="https://www.goodreads.com/user/show/108357320-jacob-johnson" id="socialIcons" target="blank" className="fa fa-book fa-xs" />
             </div>
