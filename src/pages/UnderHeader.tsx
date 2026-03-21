ee22550f9f573941b01a101f62ef66b7a7d59de0
Jacob Johnson <therealjakejohnson@gmail.com>
2026-03-21 14:29:12 -0500

    Fixed the underheader icons!

--- src/pages/UnderHeader.tsx
+++ src/pages/UnderHeader.tsx
@@ -4,11 +4,11 @@ export default function UnderHeader() {
     return (
         <div className="underHeader">
             <div className="insideFooter">
-                <a href="https://www.linkedin.com/in/jacob-johnson-05bb79169/" target="_blank" className="fa fa-linkedin" id="socialIcons" title="LinkedIn" />
-                <a href="https://github.com/JacobJohnson1" target="_blank" className="fa fa-github" id="socialIcons" title="GitHub" />
-                <a href="https://www.youtube.com/@jacob-johnson2" target="_blank" className="fa fa-youtube-play" id="socialIcons" title="YouTube" />
-                <a href="mailto:therealjakejohnson@gmail.com" className="fa fa-envelope-o" id="socialIcons" title="Email" />
-                <a href="https://www.goodreads.com/user/show/108357320-jacob-johnson" id="socialIcons" target="_blank" className="fa fa-book" title="Goodreads" />
+                <a href="https://www.linkedin.com/in/jacob-johnson-05bb79169/" target="_blank" id="socialIcons" title="LinkedIn"><i className="fab fa-linkedin-in"></i></a>
+                <a href="https://github.com/JacobJohnson1" target="_blank" id="socialIcons" title="GitHub"><i className="fab fa-github"></i></a>
+                <a href="https://www.youtube.com/@jacob-johnson2" target="_blank" id="socialIcons" title="YouTube"><i className="fab fa-youtube"></i></a>
+                <a href="mailto:therealjakejohnson@gmail.com" id="socialIcons" title="Email"><i className="fas fa-envelope"></i></a>
+                <a href="https://www.goodreads.com/user/show/108357320-jacob-johnson" id="socialIcons" target="_blank" title="Goodreads"><i className="fas fa-book"></i></a>
             </div>
         </div>
     )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
       </Head>
       <Component {...pageProps} />
     </>
