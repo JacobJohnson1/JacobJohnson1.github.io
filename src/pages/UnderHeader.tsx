8a0a328f23370dc9e2230a31aeea083205e60bed
JacobJohnson1 <70165217+JacobJohnson1@users.noreply.github.com>
2026-03-20 23:23:55 -0500

    CSS facelift, but I can't see the social icons anymore - need to fix

--- src/pages/UnderHeader.tsx
+++ src/pages/UnderHeader.tsx
@@ -3,13 +3,12 @@ import React from "react";
 export default function UnderHeader() {
     return (
         <div className="underHeader">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
             <div className="insideFooter">
-                <a href="https://www.linkedin.com/in/jacob-johnson-05bb79169/" target="blank" className="fa fa-linkedin fa-xs" id="socialIcons" />
-                <a href="https://github.com/JacobJohnson1" target="blank" className="fa fa-github fa-xs" id="socialIcons" />
-                <a href="https://www.youtube.com/@jacob-johnson2" target="blank" className="fa fa-youtube-play fa-xs" id="socialIcons" />
-                <a href="mailto: therealjakejohnson@gmail.com" className="fa fa-envelope-o fa-xs" id="socialIcons" />
-                <a href="https://www.goodreads.com/user/show/108357320-jacob-johnson" id="socialIcons" target="blank" className="fa fa-book fa-xs" />
+                <a href="https://www.linkedin.com/in/jacob-johnson-05bb79169/" target="_blank" className="fa fa-linkedin" id="socialIcons" title="LinkedIn" />
+                <a href="https://github.com/JacobJohnson1" target="_blank" className="fa fa-github" id="socialIcons" title="GitHub" />
+                <a href="https://www.youtube.com/@jacob-johnson2" target="_blank" className="fa fa-youtube-play" id="socialIcons" title="YouTube" />
+                <a href="mailto:therealjakejohnson@gmail.com" className="fa fa-envelope-o" id="socialIcons" title="Email" />
+                <a href="https://www.goodreads.com/user/show/108357320-jacob-johnson" id="socialIcons" target="_blank" className="fa fa-book" title="Goodreads" />
             </div>
         </div>
     )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
